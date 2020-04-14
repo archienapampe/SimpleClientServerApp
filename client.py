1085e69d9d72f7aed054949c6c235cc58be90257
Archie <artur.husty12@gmail.com>
2020-04-14 21:40:06 +0300

    server side bugs fixed

--- client.py
+++ client.py
@@ -1,18 +1,20 @@
 import socket
 
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 5001))
-
-message = client_socket.recv(4096).decode("utf-8")
-print(message)
-client_socket.send((input('choose your drink: ')).encode())
-client_socket.send((input('choose your ingredients: ')).encode())
-message = client_socket.recv(4096).decode("utf-8")
-print(message)
-
-
+with socket.create_connection(('localhost', 5001)) as client_socket:
+    message = client_socket.recv(4096).decode("utf-8")
+    print(message)
+    try:
+        while True:
+            drink = input('Choose your drink: ').strip()
+            if drink:
+                ingredient = input('Choose your ingredient: ').strip()
+                if ingredient:
+                    client_socket.sendall(f'{drink}\n{ingredient}'.encode())
+                    break
+            print('You did not choose anything')
+        message = client_socket.recv(4096).decode("utf-8")
+        print(message)
+    except KeyboardInterrupt:
+        client_socket.close()
         
-  
-
-
--- server.py
+++ server.py
@@ -26,7 +26,7 @@ def accept_connection(server_socket):
     while True:
             try:
                 client_socket, addr = server_socket.accept()
-                log.info(f'{client_socket=}, {addr=}')
+                log.info(f'{addr=}')
                 print('Connection from', addr)
             except socket.error:
                 print('lets buy some drink!!!')
@@ -36,37 +36,33 @@ def accept_connection(server_socket):
             else:
                 client_socket.setblocking(True)
                 send_message(client_socket)
-    
+                
 def send_message(client_socket):
     client_socket.sendall(f'{json.dumps(menu, indent=16)}'.encode())
     while True:
         try:
-            choice_drinks = client_socket.recv(4096).decode('utf-8')
-            if menu['drinks'][choice_drinks] == 0:
-                client_socket.sendall(f'Sorry, but I have no {choice_drinks}'.encode())
+            choice_drink, choice_ingredient = client_socket.recv(4096).decode('utf-8').split('\n')
+            if all((menu['drinks'][choice_drink], menu['ingredients'][choice_ingredient])):
+                menu['drinks'][choice_drink] -= 1
+                menu['ingredients'][choice_ingredient] -= 1
+                log.info(f'client ordered {choice_drink} with {choice_ingredient}')
+                client_socket.sendall(f'Take your {choice_drink} with {choice_ingredient}'.encode())
                 break
-            menu['drinks'][choice_drinks] -= 1
-            log.info(f"{choice_drinks=} | {menu['drinks'][choice_drinks]} items left")
-            
-            choice_ingredients = client_socket.recv(4096).decode('utf-8')
-            if menu['ingredients'][choice_ingredients] == 0:
-                client_socket.sendall(f'Sorry, but I have no {choice_ingredients}'.encode())
+            else:
+                client_socket.sendall(f'I have not enough {choice_drink} or {choice_ingredient}'.encode())
                 break
-            menu['ingredients'][choice_ingredients] -= 1
-            log.info(f"{choice_ingredients=} | {menu['ingredients'][choice_ingredients]} items left")
         except KeyError:
             client_socket.sendall('Sorry, I cannot make it'.encode())
             break
-        
-        client_socket.sendall(f'take your {choice_drinks} with {choice_ingredients}'.encode())
-        break
+        except ValueError:
+            break
     client_socket.close()
 
 
 if __name__ == '__main__':
     log = logging.getLogger('Coffee machine')
     log.setLevel(logging.INFO)
-    fh = logging.FileHandler('logging.log', 'w', 'utf-8')
+    fh = logging.FileHandler('ordering.log', 'w', 'utf-8')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     log.addHandler(fh)
